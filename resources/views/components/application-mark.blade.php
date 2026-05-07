805cb1a21f4579834feb9aa2c6767eafd1364326
unknown <danielhagantaginting@gmail.com>
2026-05-07 16:32:48 +0700

    logo

--- resources/views/components/application-mark.blade.php
+++ resources/views/components/application-mark.blade.php
@@ -1,4 +1,27 @@
-<svg viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg" {{ $attributes }}>
-  <path d="M11.395 44.428C4.557 40.198 0 32.632 0 24 0 10.745 10.745 0 24 0a23.891 23.891 0 0113.997 4.502c-.2 17.907-11.097 33.245-26.602 39.926z" fill="#6875F5"/>
-  <path d="M14.134 45.885A23.914 23.914 0 0024 48c13.255 0 24-10.745 24-24 0-3.516-.756-6.856-2.115-9.866-4.659 15.143-16.608 27.092-31.75 31.751z" fill="#6875F5"/>
+<svg viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg" {{ $attributes }}>
+  <defs>
+    <linearGradient id="ptwMarkGradient" x1="8" y1="6" x2="40" y2="44" gradientUnits="userSpaceOnUse">
+      <stop stop-color="#003884" />
+      <stop offset="1" stop-color="#0B5ED7" />
+    </linearGradient>
+  </defs>
+
+  <!-- Shield -->
+  <path
+    d="M24 4c7.3 0 13.2 3 16 4.9v14.4c0 10.2-6.6 17.8-16 20.7-9.4-2.9-16-10.5-16-20.7V8.9C10.8 7 16.7 4 24 4Z"
+    fill="url(#ptwMarkGradient)"
+  />
+
+  <!-- Inner cut -->
+  <path
+    d="M24 8c5.6 0 10.4 2.3 12.7 3.9v11.3c0 8.4-5 14.6-12.7 17-7.7-2.4-12.7-8.6-12.7-17V11.9C13.6 10.3 18.4 8 24 8Z"
+    fill="#FFFFFF"
+    opacity="0.14"
+  />
+
+  <!-- Bolt -->
+  <path
+    d="M26.8 12.5 17.7 26.1h6.7l-3 10.7 9.9-14.8h-6.8l2.3-9.5Z"
+    fill="#FFFFFF"
+  />
 </svg>
